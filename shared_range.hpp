4924a542e3a42cea02d06bb15170c2b1817d6242
Rhys Ulerich <rhys.ulerich@gmail.com>
2011-10-19 18:14:01 -0500

    documentation cleanups

--- shared_range.hpp
+++ shared_range.hpp
@@ -305,7 +305,7 @@ inline void swap(shared_range<T>& a, shared_range<T>& b)
 }
 
 //! Are \c l and \c r semantically equivalent?
-//  To check if the \c l and \c r are identical, use <tt>l.equal(r)</tt>.
+//  To check if \c l and \c r are identical, use <tt>l.equal(r)</tt>.
 template< class T >
 inline bool operator==(const shared_range<T>& l, const shared_range<T>& r)
 {
@@ -317,8 +317,7 @@ inline bool operator==(const shared_range<T>& l, const shared_range<T>& r)
 }
 
 //! Are \c l and \c r not semantically equivalent?
-//! Are \c l and \c r semantically equivalent?
-//  To check if the \c l and \c r are not identical, use <tt>!l.equal(r)</tt>.
+//  To check if \c l and \c r are not identical, use <tt>!l.equal(r)</tt>.
 template< class T >
 inline bool operator!=(const shared_range<T>& l, const shared_range<T>& r)
 {
